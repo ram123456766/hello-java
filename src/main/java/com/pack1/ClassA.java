9d19757efce8b3f92a3648f96564b5d0aa130620
ram123456766 <ramavidyaranyak@gmail.com>
2025-12-23 16:48:06 -0600

    Change hello message

--- src/main/java/com/pack1/ClassA.java
+++ src/main/java/com/pack1/ClassA.java
@@ -4,5 +4,7 @@ public class ClassA {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is a normal Java project");
+        System.out.println("Hello, this is a fucking Java project");
+
     }
 }
